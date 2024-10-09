827f053864f69b1b4a4d24567dfe4223ff5923f0
malik-13503 <95710499+malik-13503@users.noreply.github.com>
2024-10-09 23:59:54 +0500

    Update masterQuiz.js

--- src/js/masterQuiz.js
+++ src/js/masterQuiz.js
@@ -205,7 +205,7 @@ function endGame() {
     // Add a delay to go back to the "nec selector" page after 5 seconds
     setTimeout(() => {
       // Redirect to the "wheel" page (replace 'wheel.html' with the actual URL)
-      window.location.href = "wheel.html";
+      window.location.href = "Final-Wheel/index.html";
       // window.location.href = "https://content.republictt.com/THIS-Conference-2024_.html";
     }, 5000); // 5000 milliseconds (5 seconds)
 
@@ -215,7 +215,7 @@ function endGame() {
     // Add a delay to go back to the "nec selector" page after 5 seconds
     setTimeout(() => {
       // Redirect to the "wheel" page (replace 'wheel.html' with the actual URL)
-      window.location.href = "wheel.html";
+      window.location.href = "Final-Wheel/index.html";
       // window.location.href = "https://content.republictt.com/THIS-Conference-2024_.html";
     }, 5000); // 5000 milliseconds (5 seconds)
   }
